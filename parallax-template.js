f665b70b27b00403001f5ef024d37496df20c309
Mario Siqueira <mario@orph.eu>
2016-08-10 19:01:23 -0300

    pinegrow

---
+++ parallax-template.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.carousel.carousel-slider').carousel({full_width: true});
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
