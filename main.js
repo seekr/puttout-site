7e9d35a26779388537b60f17eb475fbe048d43a8
Mario Siqueira <mario@orph.eu>
2016-08-17 19:46:35 -0300

    restart

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+            $(document).ready(function(){
+            $('#nav').localScroll(800);
+            //.parallax(xPosition, speedFactor, outerHeight) options:
+            //xPosition - Horizontal position of the element
+            //inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
+            //outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
+            $('body').parallax("25%", 0.25, true);
+            $('#intro').parallax("25%", 0.1);
+            $('#third').parallax("25%", 0.15);
+        })
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
