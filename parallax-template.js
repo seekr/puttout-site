530527e6febca692231c21f6469af171abe24fab
Mario Siqueira <mario@orph.eu>
2016-08-17 13:09:24 -0300

    mac

--- parallax-template.js
+++ parallax-template.js
@@ -3,7 +3,6 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-    $('.carousel.carousel-slider').carousel({full_width: true});
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
